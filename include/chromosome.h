b78e1aba8f158cbf2526b58c6f77c375a7d86fc7
petter77 <ppawlinski77@gmail.com>
2026-03-25 15:06:30 +0100

    refactor: naming changes

--- include/chromosome.h
+++ include/chromosome.h
@@ -11,7 +11,7 @@ typedef struct {
 } chromosome_t;
 
 
-chromosome_t chromosome_init_random (const tsp_t *tsp, int num_of_cities);
+chromosome_t chromosome_init_random (const tsp_t *tsp);
 
 chromosome_t chromosome_crossover ( const chromosome_t *parent_a, 
                                     const chromosome_t *parent_b,
--- src/chromosome.c
+++ src/chromosome.c
@@ -4,19 +4,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int compute_fitness(const uint8_t *cities, int num_of_cities, const tsp_t *tsp) {
+int compute_fitness(const uint8_t *cities, const tsp_t *tsp) {
     int sum = 0;
-    for (int i = 0; i < num_of_cities - 1; i++) {
+    for (int i = 0; i < tsp->num_of_cities - 1; i++) {
         sum += dist_get(tsp, cities[i], cities[i + 1]);
     }
 
-    sum += dist_get(tsp, cities[num_of_cities - 1], cities[0]);
+    sum += dist_get(tsp, cities[tsp->num_of_cities - 1], cities[0]);
     return sum;
 }
 
 void shuffle (uint8_t *arr, int num_of_cities) {
-    for (int i = num_of_cities - 1; i > 0; i--) {
-        uint8_t j = arc4random_uniform(i+1);
+    for (int i = num_of_cities - 1; i > 1; i--) {
+        uint8_t j = arc4random_uniform(i) + 1;
 
         uint8_t tmp = arr[j];
         arr[j] = arr[i];
@@ -24,15 +24,15 @@ void shuffle (uint8_t *arr, int num_of_cities) {
     }
 }
 
-chromosome_t chromosome_init_random (const tsp_t *tsp, int num_of_cities) {
-    chromosome_t chromosome;
-    chromosome.cities = malloc(sizeof(uint8_t) * num_of_cities);
-    for (uint8_t i = 0; i < num_of_cities; i++) {
-        chromosome.cities[i] = i;
+chromosome_t chromosome_init_random (const tsp_t *tsp) {
+    chromosome_t chr;
+    chr.cities = malloc(sizeof(uint8_t) * tsp->num_of_cities);
+    for (uint8_t i = 0; i < tsp->num_of_cities; i++) {
+        chr.cities[i] = i;
     }
-    shuffle(chromosome.cities, num_of_cities);
-
-    return chromosome;
+    shuffle(chr.cities, tsp->num_of_cities);
+    chr.fitness = compute_fitness(chr.cities, tsp);
+    return chr;
 }
 
 /*
