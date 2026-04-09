8c477bde325cb3d1b6bd08c611e55d556ac8c249
petter77 <ppawlinski77@gmail.com>
2026-04-09 13:12:54 +0200

    add chromosome crossover function

--- src/chromosome.c
+++ src/chromosome.c
@@ -6,6 +6,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void chromosome_print (const chromosome_t *chr) {
+    for (int i = 0; i < chr->length; i++) {
+        printf("%d ", chr->cities[i]);
+    }
+    printf("\n");
+}
+
 int compute_fitness(const uint8_t *cities, const tsp_t *tsp) {
     int sum = 0;
     for (int i = 0; i < tsp->num_of_cities - 1; i++) {
@@ -76,10 +83,65 @@ chromosome_t chromosome_crossover (population_t *pop) {
     chromosome_t *parent_a = tournament_select(pop);   
     chromosome_t *parent_b = NULL;
     do {
-        chromosome_t *parent_b = tournament_select(pop);
+        parent_b = tournament_select(pop);
     } while (parent_a == parent_b);
 
+    printf("Parent A:\n");
+    chromosome_print(parent_a);
+    printf("Parent B:\n");
+    chromosome_print(parent_b);
+
+    uint8_t cut1 = arc4random_uniform(parent_a->length - 2) + 1;
+    uint8_t cut2;
+    do {
+        cut2 = arc4random_uniform(parent_a->length - 2) + 1;
+    } while (cut2 == cut1 || abs(cut2 - cut1) < 2);
+
+    if (cut1 > cut2) { uint8_t tmp = cut1; cut1 = cut2; cut2 = tmp; }
+    printf("Cut 1: %d\n", cut1);
+    printf("Cut 2: %d\n", cut2);
+
     chromosome_t child;
+    child.length = parent_a->length;
+    child.cities = malloc(sizeof(uint8_t) * child.length);
+    child.fitness = 0;
+
+    for (int i = 0; i < child.length; i++) {
+        child.cities[i] = 255;
+    }
+
+    for (uint8_t i = cut1; i <= cut2; i++) {
+        child.cities[i] = parent_a->cities[i];
+    }
+
+    uint8_t missing = (child.length - (cut2 - cut1) - 1);
+    uint8_t missing_arr[missing];
+
+    int missing_index = 0;
+    for (int i = 0; i < child.length; i++) {
+        bool present = false;
+        for (int j = 0; j < child.length; j++) {
+            if (parent_b->cities[i] == child.cities[j]) {
+                present = true;
+                break;
+            }
+        }
+        if (!present) { 
+            missing_arr[missing_index] = parent_b->cities[i];
+            missing_index++;
+        }
+    }
+    for (int j = 0; j < missing; j++) {
+        for (int i = 0; i < child.length; i++){
+            if (child.cities[i] == 255) {
+                child.cities[i] = missing_arr[j];
+                break;
+            }
+        }
+    }
+
+    printf("child:\n");
+    chromosome_print(&child);
     return child;
 }
 /*
@@ -93,9 +155,3 @@ void chromosome_free (chromosome_t *chr) {
     free(chr);
 }
 
-void chromosome_print (const chromosome_t *chr) {
-    for (int i = 0; i < chr->length; i++) {
-        printf("%d", chr->cities[i]);
-    }
-    printf("\n");
-}
--- src/main.c
+++ src/main.c
@@ -1,15 +1,27 @@
 #include "../include/config.h"
 #include "../include/tsp.h"
 #include "../include/population.h"
+#include "../include/log.h"
+
+#include <stdlib.h>
 
 int main (int argc, char *argv[]) {
     config_t config = config_parse(argc, argv);
     tsp_t tsp = tsp_initialize(&config);
 
     population_t population = population_init (&config, &tsp);
-    population_print(&population);
+    //population_print(&population);
+
+
+
+    chromosome_t child = chromosome_crossover(&population);
+    chromosome_t *child1 = malloc(sizeof(chromosome_t));
+    *child1 = child;
 
     dist_arr_free(&tsp);
+    chromosome_free(child1);
     population_free (&population);
+    LOG_OK("Program finished!");
+    
     return 0;
 }
